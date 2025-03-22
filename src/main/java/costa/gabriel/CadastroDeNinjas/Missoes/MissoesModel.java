5cba3ba5c49ddae594ee6c91a7c32d42c569d1a8
grbrielcosta <gvbrielcosta@gmail.com>
2025-03-22 18:32:51 -0300

    fix: inclui a anotation @JsonIgnore no MissoesModel para previnir erro de serializacao

--- src/main/java/costa/gabriel/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/costa/gabriel/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package costa.gabriel.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import costa.gabriel.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -25,6 +26,7 @@ public class MissoesModel {
 
     // @OneToMany - Uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 
 
