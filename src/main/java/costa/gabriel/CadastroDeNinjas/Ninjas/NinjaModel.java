723e935390522eaa0b84bd89f9d9051c4386d9a6
grbrielcosta <gvbrielcosta@gmail.com>
2025-03-22 18:33:34 -0300

    feat: alterar rota do controller para listar os ninjas e injeta a dependencia do service e do repository

--- src/main/java/costa/gabriel/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/costa/gabriel/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -33,44 +33,4 @@ public class NinjaModel {
     private MissoesModel missoes;
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public MissoesModel getMissoes() {
-        return missoes;
-    }
-
-    public void setMissoes(MissoesModel missoes) {
-        this.missoes = missoes;
-    }
-
 }
