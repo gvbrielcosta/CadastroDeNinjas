03911fc19d7a5d575677f613bd94cd71dd5b7435
grbrielcosta <gvbrielcosta@gmail.com>
2025-03-24 20:30:13 -0300

    chore: cria a versao do dto pela entidade e mapeia manualmente no ninjamapper os caminhos entidade x dto

--- src/main/java/costa/gabriel/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/costa/gabriel/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package costa.gabriel.CadastroDeNinjas.Ninjas;
 
+import costa.gabriel.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
+
+
 }
