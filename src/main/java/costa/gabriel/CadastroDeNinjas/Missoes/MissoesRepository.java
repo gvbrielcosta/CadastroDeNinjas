6ed9f19b69c42fa25f109bc10189bbcf8f504e7e
grbrielcosta <gvbrielcosta@gmail.com>
2025-03-24 20:30:51 -0300

    feat: inclui dto como abstraçao para entidade na camada de service e controller

--- src/main/java/costa/gabriel/CadastroDeNinjas/Missoes/MissoesRepository.java
+++ src/main/java/costa/gabriel/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -1,7 +1,9 @@
 package costa.gabriel.CadastroDeNinjas.Missoes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
 
 }
