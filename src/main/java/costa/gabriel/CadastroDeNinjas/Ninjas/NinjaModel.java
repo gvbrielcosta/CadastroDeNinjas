65d1ae7dad6c736581f32824c1b3468852430c55
grbrielcosta <gvbrielcosta@gmail.com>
2025-03-24 20:28:05 -0300

    fix: inclui a coluna rank na entidade ninjamodel, porem ja estava incluida na migrationv2

--- src/main/java/costa/gabriel/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/costa/gabriel/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -28,6 +28,9 @@ public class NinjaModel {
     @Column(name = "idade")
     private int idade;
 
+    @Column(name="rank")
+    private String rank;
+
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
