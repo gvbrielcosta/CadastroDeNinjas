8f858342d9c89c1edd254842c5c6a00cba5600ca
grbrielcosta <gvbrielcosta@gmail.com>
2025-04-02 20:14:26 -0300

    doc: adiciona o swagger na aplicação para documentação das rotas no controller de ninjas

--- src/main/java/costa/gabriel/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/costa/gabriel/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,5 +1,9 @@
 package costa.gabriel.CadastroDeNinjas.Ninjas;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,11 @@ public class NinjaController {
     }
 
     @PostMapping("/criar")
+    @Operation(summary = "Rota para criar ninjas", description = "Essa rota cria um ninja")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Ninja criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na criação do ninja")
+    })
     public ResponseEntity<String> criarNinja (@RequestBody NinjaDTO ninja) {
         NinjaDTO novoNinja = ninjaService.criarNinja(ninja);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -24,11 +33,21 @@ public class NinjaController {
     }
 
     @GetMapping("/listar")
+    @Operation(summary = "Rota para listar", description = "Essa rota lista todos os ninjas no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninjas listados com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro para listar ninjas")
+    })
     public ResponseEntity<List<NinjaDTO>> listarNinjas(){
         return ResponseEntity.ok(ninjaService.listarNinjas());
     }
 
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Rota para listar ninja por ID", description = "Essa rota lista um único ninja específico pelo seu ID (passado na url)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja listado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro para listar ninja")
+    })
     public ResponseEntity<?> listarNinjasPorId(@PathVariable Long id){
         NinjaDTO ninja = ninjaService.listarNinjasPorId(id);
         if (ninja != null) {
@@ -39,7 +58,15 @@ public class NinjaController {
     }
 
     @PutMapping("/alterar/{id}")
-    public ResponseEntity<?> alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) {
+    @Operation(summary = "Rota para alterar ninja", description = "Essa rota lista altera um ninja pelo seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja alterado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro para alterar ninja")
+    })
+    public ResponseEntity<?> alterarNinjaPorId(
+            @Parameter(description = "Usuário envia o ID no caminho da requisição")
+            @PathVariable Long id,
+            @RequestBody NinjaDTO ninjaAtualizado) {
         NinjaDTO ninja = ninjaService.atualizarNinja(id, ninjaAtualizado);
         if (ninja != null) {
             return ResponseEntity.ok(ninja);
@@ -49,6 +76,11 @@ public class NinjaController {
     }
 
     @DeleteMapping("/deletar/{id}")
+    @Operation(summary = "Rota para deletar ninja", description = "Essa rota deleta um ninja pelo seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja deletado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro para deletar ninja")
+    })
     public ResponseEntity<String> deletarNinjaPorId(@PathVariable Long id){
         if (ninjaService.listarNinjasPorId(id) != null) {
             NinjaDTO ninjaParaDeletar = ninjaService.listarNinjasPorId(id);
---
+++ src/main/java/costa/gabriel/CadastroDeNinjas/Ninjas/NinjaControllerUi.java
@@ -0,0 +1,26 @@
+package costa.gabriel.CadastroDeNinjas.Ninjas;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninjas/ui")
+public class NinjaControllerUi {
+
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUi(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/listar")
+    public String listarNinjas(Model model) {
+        model.addAttribute("ninjas", ninjaService.listarNinjas());
+        return "listarNinjas";
+    }
+}
